c1b625b7008d840181ab72fde57125212a764459
schueler <schueler@ubuntu>
2021-06-18 11:08:23 +0200

    init myTeam

---
+++ MyTeam/src/pkgData/Player.java
@@ -0,0 +1,39 @@
+package pkgData;
+
+public class Player {
+	private int numberPlayer;
+	private String namePlayer, position;
+	public Player(int numberPlayer, String namePlayer, String position) {
+		super();
+		this.numberPlayer = numberPlayer;
+		this.namePlayer = namePlayer;
+		this.position = position;
+	}
+	public int getNumberPlayer() {
+		return numberPlayer;
+	}
+	public void setNumberPlayer(int numberPlayer) {
+		this.numberPlayer = numberPlayer;
+	}
+	public String getNamePlayer() {
+		return namePlayer;
+	}
+	public void setNamePlayer(String namePlayer) {
+		this.namePlayer = namePlayer;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	
+	@Override
+	public String toString() {
+		return "Player [numberPlayer=" + numberPlayer + ", namePlayer=" + namePlayer + ", position=" + position + "]";
+	}
+	
+	
+	
+
+}
